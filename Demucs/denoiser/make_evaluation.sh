d8dfbb4b89529f3b3926c0d2d1aeca9e6cc3b3d9
Yunyang Zeng <yunyangzeng@gmail.com>
2023-01-22 20:03:18 -0500

    Delete make_evaluation.sh

--- Demucs/denoiser/make_evaluation.sh
+++
@@ -1,2 +0,0 @@
-python3 -m denoiser.audio /home/yunyangz/Documents/test_set/test_set/real_recordings/ > egs/eval_real_recordings/clean.json
-#python3 -m denoiser.audio /home/yunyangz/Documents/test_set/test_set/synthetic/no_reverb/noisy > egs/eval/tt/noisy.json
