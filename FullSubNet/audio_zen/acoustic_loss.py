75def6a8f1634a0a7c6abaa39d155637e5d8d390
Yunyang Zeng <yunyangzeng@gmail.com>
2023-02-15 17:23:13 -0800

    Update acoustic_loss.py

--- FullSubNet/audio_zen/acoustic_loss.py
+++ FullSubNet/audio_zen/acoustic_loss.py
@@ -4,9 +4,7 @@ import torch.nn.functional as F
 
 
 class AcousticLoss(torch.nn.Module):
-    
     def __init__(self, args, device = 'cuda'):
-        
         super(AcousticLoss, self).__init__()
         self.args = args
         self.device = device
@@ -20,29 +18,19 @@ class AcousticLoss(torch.nn.Module):
             self.l1 = torch.nn.L1Loss()
         self.estimate_acoustics.load_state_dict(model_state_dict)
         self.estimate_acoustics.to(device)
-            
-        
+                
     def __call__(self, clean_waveform, enhan_wav_waveform, mode = "train"):
-        
         return self.forward(clean_waveform, enhan_waveform, mode)
 
-    def forward(self, clean_waveform, enhan_waveform, mode):
-        """ 
-            clean_stft => (B, T, F * 2) 
-            enhan_stft => (B, T, F * 2)
-            noisy_stft => (B, T, F * 2)       
-        """         
+    def forward(self, clean_waveform, enhan_waveform, mode):    
         if mode == "train":
             self.estimate_acoustics.train()
         else:
-            self.estimate_acoustics.eval()
-            
-            
+            self.estimate_acoustics.eval()    
         clean_spectrogram = self.get_stft(clean_waveform)
         enhan_spectrogram,enhan_st_energy = self.get_stft(enhan_waveform, return_short_time_energy = True)        
         clean_acoustics = self.estimate_acoustics(clean_spectrogram)
-        enhan_acoustics = self.estimate_acoustics(enhan_spectrogram)
-        
+        enhan_acoustics = self.estimate_acoustics(enhan_spectrogram)      
         loss_type = self.args["acoustic_loss"]["type"]
         if self.loss_type == "l2":
             acoustic_loss   = self.l2(enhan_acoustics, clean_acoustics)
@@ -54,7 +42,6 @@ class AcousticLoss(torch.nn.Module):
         elif self.loss_type == "frame_energy_weighted_l1":
             acoustic_loss   = torch.mean(torch.sigmoid(enhan_st_energy).unsqueeze(dim = -1) \
             * torch.abs(enhan_acoustics - clean_acoustics))
-
         return acoustic_loss
     
     def get_stft(self, wav, return_short_time_energy = False):
@@ -64,7 +51,6 @@ class AcousticLoss(torch.nn.Module):
         spec_real = spec[..., 0]
         spec_imag = spec[..., 1]     
         spec = spec.permute(0, 2, 1, 3).reshape(spec.size(dim=0), -1, 2 * (self.nfft//2 + 1))
-
         if return_short_time_energy:
             st_energy = torch.mul(torch.sum(spec_real**2 + spec_imag**2, dim = 1), 2/self.nfft)
             assert spec.size(dim=1) == st_energy.size(dim=1)
@@ -74,11 +60,8 @@ class AcousticLoss(torch.nn.Module):
                   
                                                  
 class AcousticEstimator(torch.nn.Module):
-    
     def __init__(self):
-        
         super(AcousticEstimator, self).__init__()
-        
         self.lstm = torch.nn.LSTM(514, 256, 3, bidirectional=True, batch_first=True)
         self.linear1 = torch.nn.Linear(512, 256)
         self.linear2 = torch.nn.Linear(256, 25)
@@ -89,6 +72,5 @@ class AcousticEstimator(torch.nn.Module):
         Z1    = self.linear1(A1)
         A2    = self.act(Z1)
         Z2    = self.linear2(A2)
-        
         return Z2
 
