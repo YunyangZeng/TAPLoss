4236497b1038ad0a0b0d19b225f576688c8ecb33
Yunyang Zeng <yunyangzeng@gmail.com>
2023-03-09 14:39:54 -0800

    Update TAPLoss.py
    Add comments

--- TAPLoss/TAPLoss.py
+++ TAPLoss/TAPLoss.py
@@ -4,13 +4,24 @@ import sys
 import torch.nn.functional as F
 import numpy as np
 from TAP_estimator import AcousticEstimator
+from typing import *
 
 class AcousticLoss(torch.nn.Module):
-    def __init__(self, loss_type, acoustic_model_path, paap = False, paap_weight_path = None, device = 'cuda'):
+    def __init__(self, loss_type: str, acoustic_model_path: str, paap: bool = False, paap_weight_path: str = None, device = 'cuda'):
+        """
+        Args:
+            loss_type (str):
+                Must be one of the following 4 options: ["l2", "l1", "frame_energy_weighted_l2", "frame_energy_weighted_l1"]
+            acoustic_model_path (str):
+                Path to the pretrained temporal acoustic parameter estimator model checkpoint.
+            paap (bool):
+                True for use PAAPLoss, False for use TAPLoss.
+            paap_weight_path (str):
+                Path to the Paap weight .npy file
+        """
         super(AcousticLoss, self).__init__()
-        self.device      = device
-        self.paap        = paap
-        model_state_dict = torch.load(acoustic_model_path, map_location=device)['model_state_dict']
+        self.device       = device
+        self.paap         = paap
         self.estimate_acoustics = AcousticEstimator()
         self.loss_type = loss_type
         if self.loss_type == "l2":
@@ -22,25 +33,45 @@ class AcousticLoss(torch.nn.Module):
                 raise ValueError("PAAP weight path is not provided")
             else:
                 self.paap_weight = torch.from_numpy(np.load(paap_weight_path)).to(device)
+        
+        model_state_dict  = torch.load(acoustic_model_path, map_location=device)['model_state_dict']
         self.estimate_acoustics.load_state_dict(model_state_dict)
         self.estimate_acoustics.to(device)
         
     def __call__(self, clean_waveform, enhan_waveform, mode="train"):
         return self.forward(clean_waveform, enhan_waveform, mode)
 
-    def forward(self, clean_waveform, enhan_waveform, mode):
-        if mode == "train":
+    def forward(self, clean_waveform: torch.FloatTensor, enhan_waveform: torch.FloatTensor, mode: str) -> torch.FloatTensor:
+
+        """
+        Args:
+            clean_waveform (torch.FloatTensor)：
+                Tensor of clean waveform with shape (B, T * sr).
+            enhan_waveform (torch.FloatTensor)： 
+                Tensor of enhanced waveform with shape (B, T * sr).
+            mode (str) : 
+                'train' or 'eval'
+        Returns:
+            acoustic_loss (torch.FloatTensor):
+                Loss value corresponding to the selected loss type.
+        """
+        
+        if   mode == "train":
             self.estimate_acoustics.train()
+        elif mode == "eval":
+            self.estimate_acoustics.eval()     
         else:
-            self.estimate_acoustics.eval()        
+            raise ValueError("Invalid mode, must be either 'train' or 'eval'.")
+            
+            
         clean_spectrogram = self.get_stft(clean_waveform)
         enhan_spectrogram, enhan_st_energy = self.get_stft(enhan_waveform, return_short_time_energy = True)
         clean_acoustics   = self.estimate_acoustics(clean_spectrogram)
         enhan_acoustics   = self.estimate_acoustics(enhan_spectrogram)
         if self.paap:
             """
-                paap_weight ==> (26, 40) 
-                acoustics ==> (B, T, 25), expand last dimension by 1 for bias
+            paap_weight ==> (26, 40) 
+            acoustics ==> (B, T * sr, 25), expand last dimension by 1 for bias
             """
             clean_acoustics = torch.cat((clean_acoustics, torch.ones(clean_acoustics.size(dim = 0),\
                         clean_acoustics.size(dim = 1), 1, device = self.device)), dim = -1) # acoustics ==> (B, T, 26)
@@ -65,24 +96,38 @@ class AcousticLoss(torch.nn.Module):
             acoustic_loss   = torch.mean(torch.sigmoid(enhan_st_energy).unsqueeze(dim = -1) \
             * torch.abs(enhan_acoustics - clean_acoustics))
         else:
-            raise ValueError(f"Invalid loss_type {self.loss_type}")
+            raise ValueError("Invalid loss_type {}".format(self.loss_type))
+    
         return acoustic_loss            
            
     
-    def get_stft(self, wav, return_short_time_energy = False):
+    def get_stft(self, wav: torch.FloatTensor, return_short_time_energy: bool = False) -> torch.FloatTensor:
+        """
+        Args:
+            wav (torch.FloatTensor):
+                Tensor of waveform of shape: (B, T * sr).
+            return_short_time_energy (bool):
+                True to return both complex spectrogram and short-time energy.
+        Returns:
+            spec (torch.FloatTensor): 
+                Real value representation of complex spectrograms, real part \
+                and imag part alternate along the frequency axis. 
+            st_energy (torch.FloatTensor):
+                Short-time energy calculated in the frequency domain using Parseval's theorem.  
+        """
         self.nfft       = 512
         self.hop_length = 160
-        spec = torch.stft(wav, n_fft=self.nfft, hop_length=self.hop_length, return_complex=False)
+        spec = torch.stft(wav, n_fft=self.nfft, hop_length=self.hop_length, return_complex=False) # Rectangular window with window length = 32ms, hop length = 10ms
         spec_real = spec[..., 0]
         spec_imag = spec[..., 1]     
-        spec = spec.permute(0, 2, 1, 3).reshape(spec.size(dim=0), -1,  2 * (self.nfft//2 + 1))
+        spec = spec.permute(0, 2, 1, 3).reshape(spec.size(dim=0), -1,  2 * (self.nfft//2 + 1)) # spec ==> (B, T * sr, 2 * (nfft / 2 + 1))
 
         if return_short_time_energy:
-            st_energy = torch.mul(torch.sum(spec_real**2 + spec_imag**2, dim = 1), 2/self.nfft)
-            assert spec.size(dim=1) == st_energy.size(dim=1)
+            st_energy = torch.mul(torch.sum(spec_real**2 + spec_imag**2, dim = 1), 2/self.nfft) 
             return spec.float(), st_energy.float()
         else: 
             return spec.float()
+        
         
 
 
